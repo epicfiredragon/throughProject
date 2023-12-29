c9c05c06ae260ada545c77a09cd8c9d83094d03b
epicfiredragon <karpovichnikita05@gmail.com>
2023-12-29 21:26:32 +0300

    start

---
+++ cmake-build-debug/CMakeFiles/through.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/through.dir/main.cpp.o"
+  "CMakeFiles/through.dir/main.cpp.o.d"
+  "through"
+  "through.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/through.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/through.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for through.
+# This may be replaced when dependencies are built.

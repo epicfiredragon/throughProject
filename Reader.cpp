9de7faffeeb4d62a1cac426736fafb455a144535
epicfiredragon <karpovichnikita05@gmail.com>
2024-01-13 18:42:19 +0300

    fix more minor bugs

--- Reader.cpp
+++ Reader.cpp
@@ -1,30 +1,54 @@
 #include "Reader.h"
+#include <fstream>
 
 class TextReader : public Reader {
+    std::ifstream stream;
+    bool ended;
 public:
-    explicit TextReader(const std::string &filename);
+    explicit TextReader(const std::string &filename): stream(filename) {
+        ended = (stream.peek() != EOF);
+    }
 
-    std::string ReadNextLine() override;
+    std::string ReadNextLine() override {
+        std::string ret;
+        std::getline(stream, ret);
+        ended = (stream.peek() != EOF);
+        return ret;
+    }
 
-    bool IsEnd() override;
+    bool IsEnd() override {
+        return ended;
+    }
 };
 
 class XMLReader : public Reader {
 public:
-    explicit XMLReader(const std::string &filename);
+    explicit XMLReader(const std::string &filename) {
 
-    std::string ReadNextLine() override;
+    }
 
-    bool IsEnd() override;
+    std::string ReadNextLine() override {
+
+    }
+
+    bool IsEnd() override {
+
+    }
 };
 
 class JSONReader : public Reader {
 public:
-    explicit JSONReader(const std::string &filename);
+    explicit JSONReader(const std::string &filename) {
 
-    std::string ReadNextLine() override;
+    }
 
-    bool IsEnd() override;
+    std::string ReadNextLine() override {
+
+    }
+
+    bool IsEnd() override {
+
+    }
 };
 
 std::shared_ptr<Reader> ChooseReader(TypeFile type, const std::string &in_file_name) {
@@ -36,34 +60,4 @@ std::shared_ptr<Reader> ChooseReader(TypeFile type, const std::string &in_file_n
         case TypeFile::JSON:
             return std::make_shared<JSONReader>(in_file_name);
     }
-}
-
-TextReader::TextReader(const std::string &filename) {}
-
-std::string TextReader::ReadNextLine() {
-    return "";
-}
-
-bool TextReader::IsEnd() {
-    return true;
-}
-
-XMLReader::XMLReader(const std::string &filename) {}
-
-std::string XMLReader::ReadNextLine() {
-    return "";
-}
-
-bool XMLReader::IsEnd() {
-    return true;
-}
-
-JSONReader::JSONReader(const std::string &filename) {}
-
-std::string JSONReader::ReadNextLine() {
-    return "";
-}
-
-bool JSONReader::IsEnd() {
-    return true;
 }
--- Writer.cpp
+++ Writer.cpp
@@ -1,37 +1,37 @@
+#include <fstream>
 #include "Writer.h"
 
 class TextWriter : public Writer {
+    std::ofstream stream;
 public:
-    explicit TextWriter(const std::string &filename);
+    explicit TextWriter(const std::string &filename) : stream(filename) {}
 
-    void WriteLine(const std::string &) override;
+    void WriteLine(const std::string &line) override {
+        stream << line << std::endl;
+    }
 };
 
 class XMLWriter : public Writer {
 public:
-    explicit XMLWriter(const std::string &filename);
+    explicit XMLWriter(const std::string &filename) {
 
-    void WriteLine(const std::string &) override;
+    }
+
+    void WriteLine(const std::string &) override {
+
+    }
 };
 
 class JSONWriter : public Writer {
 public:
-    explicit JSONWriter(const std::string &filename);
+    explicit JSONWriter(const std::string &filename) {
 
-    void WriteLine(const std::string &) override;
+    }
+
+    void WriteLine(const std::string &) override {
+
+    }
 };
-
-TextWriter::TextWriter(const std::string &filename) {}
-
-void TextWriter::WriteLine(const std::string &) {}
-
-XMLWriter::XMLWriter(const std::string &filename) {}
-
-void XMLWriter::WriteLine(const std::string &) {}
-
-JSONWriter::JSONWriter(const std::string &filename) {}
-
-void JSONWriter::WriteLine(const std::string &) {}
 
 std::shared_ptr<Writer> ChooseWriter(TypeFile type, const std::string &out_file_name) {
     switch (type) {

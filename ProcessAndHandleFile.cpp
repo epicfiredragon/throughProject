0769690e4db1e27477e5b85389f6040984b6a944
[email protected] <[email protected]>
2024-01-15 16:48:29 +0300

    added isValidExpression function

--- ProcessAndHandleFile.cpp
+++ ProcessAndHandleFile.cpp
@@ -2,6 +2,7 @@
 #include "exeptions.h"
 #include <memory>
 #include <vector>
+#include <regex>
 #include "Writer.h"
 #include "Reader.h"
 #include "Solver.h"
@@ -13,14 +14,65 @@ struct markedSubstring {
     std::string str;
     bool is_arithmetic = false;
 };
-
-std::vector<markedSubstring> MarkArithmetic(const std::string &str) {
-    /*
-    * TODO : Dima
-     * эта хуйня должна бить строку в подстроки, если это выражение, то кидай подстроку в структуру и ставь тру в is_arithmetic, иначе просто кидай
-    */
+struct markedSubstring {
+      std::string str;
+      bool is_arithmetic = false;
+       markedSubstring(const std::string& s, bool is_arith) : str(s), is_arithmetic(is_arith) {}
+};
+bool isValidExpression(const std::string& expression) {
+    if(expression.size() < 3){
+        return false;
+    }
+    std::stack<char> parenthesesStack;
+    for (char ch : expression) {
+        if (std::isdigit(ch) || ch == '*' || ch == '+' || ch == '-' || ch == '/' || ch == '^'){
+        } else if (ch == '(') {
+            parenthesesStack.push(ch);
+        } else if (ch == ')') {
+            if (parenthesesStack.empty() || parenthesesStack.top() != '(') {
+                return false;
+            }
+            parenthesesStack.pop();
+        } else {
+            return false;
+        }
+    }
+    return parenthesesStack.empty();
 }
-
+std::vector<markedSubstring> MarkArithmetic(const std::string &str){
+    std::vector<std::string> arithmeticstring;
+    std::int64_t mxpos = -1;
+    for(std::int64_t i = 0 ; i < str.size() ; i++){
+        for(std::int64_t j = 1 ; j <= str.size() - i ; j++){
+            if(isValidExpression(str.substr(i , j)))mxpos = j;
+        }
+        if(mxpos != -1){
+            arithmeticstring.push_back(str.substr(i , mxpos));
+            if(i + mxpos >= str.size())break;
+            i += mxpos;
+        }
+        mxpos = -1;
+    }
+    std::int64_t temp = 0;
+    mxpos = 0;
+    bool badalloc = false;
+    for(std::int64_t i = 0 ; i < str.size() ; i++){
+        if(str.substr(i , arithmeticstring[temp].size()) == arithmeticstring[t]){
+           markedSubstring(str.substr(mxpos , i - mxpos + 1) , false);
+           mxpos = i;
+           markedSubstring(str.substr(i , arithmeticstring[temp].size()) , true);
+           if(i + arithmeticstring[temp].size() >= str.size()){
+              badalloc = true;
+              break;
+           }
+           i += arithmeticstring[temp].size();
+           temp++;
+        }
+    }
+    if(!badalloc){
+       markedSubstring(str.substr(mxpos + arithmeticstring.back().size() + 1 , str.size() - mxpos + 1) , false);
+    }
+}
 void
 HandleProcessedFile(const std::shared_ptr<Reader> &reader, const std::shared_ptr<Writer> &writer,
                     const std::shared_ptr<ArithmeticSolver> &solver) {

27027fcbcb0e48e3fedfb5172146f4a9c65fc471
[email protected] <[email protected]>
2024-01-16 12:16:36 +0300

    add tests for ProcessAndHandleFile

--- tests/testProcessAndHandle.cpp
+++ tests/testProcessAndHandle.cpp
@@ -16,15 +16,102 @@
 #include <sstream>
 #include <fstream>
 
-TEST_CASE("ArithmeticSolver Test", "[ArithmeticSolver]") {
-    std::string inputExpression = "444kld5+6";
-    std::string expectedOutput = "444kld11";
+TEST_CASE("ProcessAndHandleFile Test", "[ProcessAndHandleFile]") {
+    { 
+    FileInfo info;
+    info.in_file_name = "test_input.txt";  
+    info.out_file_name = "test_output.txt";  
+    info.in_type = TypeFile::Text;
+    info.out_type = TypeFile::Text;
+    info.pre_steps = {};  
+    info.post_steps = {}; 
 
-    
-    auto solver_type = SolverType::My;
-    auto solver = ChooseSolver(solver_type);
-    std::string result = solver->Solve(inputExpression);
+    std::ofstream testInput(info.in_file_name);
+    if (!testInput.is_open()) {
+        FAIL("Failed to create test input file");
+    }
+    testInput << "444kld5+6" << std::endl;
+    testInput.close();
 
-    REQUIRE(result == expectedOutput);
+    REQUIRE_NOTHROW(ProcessAndHandleFile(info, SolverType::My));
+
+    std::ifstream testOutput(info.out_file_name);
+    if (!testOutput.is_open()) {
+        FAIL("Failed to open test output file");
+    }
+
+    std::string result;
+    std::getline(testOutput, result);
+    testOutput.close();
+
+    REQUIRE(result == "444kld11");
+    }
+    {
+        FileInfo info;
+        info.in_file_name = "test_input2.txt";  
+        info.out_file_name = "test_output2.txt";  
+        info.in_type = TypeFile::Text;
+        info.out_type = TypeFile::Text;
+        info.pre_steps = {};  
+        info.post_steps = {};  
+
+       
+        std::ofstream testInput(info.in_file_name);
+        if (!testInput.is_open()) {
+            FAIL("Failed to create test input file");
+        }
+        testInput << "3+5*2" << std::endl;
+        testInput.close();
+
+        
+        REQUIRE_NOTHROW(ProcessAndHandleFile(info, SolverType::My));
+
+        
+        std::ifstream testOutput(info.out_file_name);
+        if (!testOutput.is_open()) {
+            FAIL("Failed to open test output file");
+        }
+
+        std::string result;
+        std::getline(testOutput, result);
+        testOutput.close();
+
+        
+        REQUIRE(result == "13");
+    }
+    {
+        
+        FileInfo info;
+        info.in_file_name = "test_input2.txt";  
+        info.out_file_name = "test_output2.txt";  
+        info.in_type = TypeFile::Text;
+        info.out_type = TypeFile::Text;
+        info.pre_steps = {};  
+        info.post_steps = {};  
+
+         std::ofstream testInput(info.in_file_name);
+        if (!testInput.is_open()) {
+            FAIL("Failed to create test input file");
+        }
+        testInput << "ff" << std::endl;
+        testInput.close();
+
+        
+        REQUIRE_NOTHROW(ProcessAndHandleFile(info, SolverType::My));
+
+        
+        std::ifstream testOutput(info.out_file_name);
+        if (!testOutput.is_open()) {
+            FAIL("Failed to open test output file");
+        }
+
+        std::string result;
+        std::getline(testOutput, result);
+        testOutput.close();
+
+        
+        REQUIRE(result == "ff");
+    }
 }
+
 

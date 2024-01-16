3b486e2977fcdadfb2be21aaa0d06e01e7ca5a56
epicfiredragon <karpovichnikita05@gmail.com>
2024-01-16 04:12:24 +0300

    add tests

---
+++ tests/testSolver.cpp
@@ -0,0 +1,8 @@
+#include "catch2/catch_test_macros.hpp"
+#include "Solver.h"
+
+TEST_CASE("Simple") {
+    auto solver_type = SolverType::My;
+    auto solver = ChooseSolver(solver_type);
+    REQUIRE(solver->Solve("25+25") == "50");
+}

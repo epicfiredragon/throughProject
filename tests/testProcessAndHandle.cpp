21dcaf988bcc5a15b346fc8e781f650d3c9921ca
[email protected] <[email protected]>
2024-01-16 12:03:33 +0300

    add test file

--- tests/testProcessAndHandle.cpp
+++ tests/testProcessAndHandle.cpp
@@ -1,2 +1,30 @@
 #include "catch2/catch_test_macros.hpp"
+#include "ProcessAndHandleFile.h"
+#include "exeptions.h"
+#include <memory>
+#include <utility>
+#include <vector>
+#include <regex>
+#include "Writer.h"
+#include "Reader.h"
 #include "Solver.h"
+#include "FileInfo.h"
+#include "FileProcessor.h"
+#include "key.h"
+#include "TypeFile.h"
+#include <Poco/StreamCopier.h>
+#include <sstream>
+#include <fstream>
+
+TEST_CASE("ArithmeticSolver Test", "[ArithmeticSolver]") {
+    std::string inputExpression = "444kld5+6";
+    std::string expectedOutput = "444kld11";
+
+    
+    auto solver_type = SolverType::My;
+    auto solver = ChooseSolver(solver_type);
+    std::string result = solver->Solve(inputExpression);
+
+    REQUIRE(result == expectedOutput);
+}
+

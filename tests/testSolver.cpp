546de885cf7eacd9fe565f804115f693a2b53ab9
[email protected] <[email protected]>
2024-01-16 05:10:26 +0300

    add tests

--- tests/testSolver.cpp
+++ tests/testSolver.cpp
@@ -1,8 +1,32 @@
 #include "catch2/catch_test_macros.hpp"
 #include "Solver.h"
 
-TEST_CASE("Simple") {
+TEST_CASE("Test1") {
     auto solver_type = SolverType::My;
     auto solver = ChooseSolver(solver_type);
-    REQUIRE(solver->Solve("25+25") == "50");
+    REQUIRE(solver->Solve("5+1") == "6");
+}
+
+TEST_CASE("Test2") {
+    auto solver_type = SolverType::My;
+    auto solver = ChooseSolver(solver_type);
+    REQUIRE(solver->Solve("(2+25*2-1^0)/2") == "25.500000");
+}
+
+TEST_CASE("Test3") {
+    auto solver_type = SolverType::My;
+    auto solver = ChooseSolver(solver_type);
+    REQUIRE(solver->Solve("1/0") == "inf");
+}
+
+TEST_CASE("Test4") {
+    auto solver_type = SolverType::My;
+    auto solver = ChooseSolver(solver_type);
+    REQUIRE(solver->Solve("5-10") == "-5");
+}
+
+TEST_CASE("Test5") {
+    auto solver_type = SolverType::My;
+    auto solver = ChooseSolver(solver_type);
+    REQUIRE(solver->Solve("2147483646+1") == "2147483647");
 }
